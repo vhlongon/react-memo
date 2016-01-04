a7af1a6d1ce0bd39fa2e8ab9958b5704c2341615
Victor Longon <viclon@lbi.local>
2016-01-04 15:36:56 +0100

    first commit

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,19 @@
-let test = 'Hi, from ES2015 with Babel';
-console.log(test);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <h2 className="app__title">
+        {this.props.title}
+        </h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+	<App title="A react component"/>,
+	document.querySelector('.react-root')
+);
