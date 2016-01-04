44992249bbcc2a4e241c2dddfffe2ee107b744cc
Victor Longon <viclon@lbi.local>
2016-01-04 18:55:38 +0100

    add presets on right order on. babelrc, update eslintrc, app. Add form component (empty), add a helper to perform ajax requests

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import Card from './Card';
+import loadAjax from './helpers/ajax';
+
 import ReactDOM from 'react-dom';
 
 export default class GameTable extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {data: ''};
+  }
+  loadData = () => {
+    let baseUrl = 'http://api.giphy.com/v1/gifs/search?q',
+      keyword = 'star+wars',
+      apikey = 'dc6zaTOxFJmzC',
+      limit = '2';
+    loadAjax(`${baseUrl}=${keyword}&api_key=${apikey}&limit=${limit}`,
+      (xhr) => {
+        this.setState({data: xhr.responseText});
+        console.log(JSON.parse(xhr.responseText));
+      }
+    );
+  }
+  componentDidMount() {
+    this.loadData();
+  }
+  render = () => {
     return (
       <div className="gametable">
         <h2 className="gametale__title">
         {this.props.title}
         </h2>
+        <div className="result">
+        {this.state.data}
+        </div>
         <Card title="A card"/>
       </div>
     );
